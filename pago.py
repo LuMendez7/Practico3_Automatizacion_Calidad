12109afa02541e6ef069994901e795f1a658fcfa
Lu Mendez <lucianomendezok@gmail.com>
2026-09-18 06:20:25 -0300

    Mejora de calidad y ampliacion de pruebas

--- pago.py
+++ pago.py
@@ -10,3 +10,13 @@ def procesar_pago(monto, saldo):
 
 # Funcion utilizada para las pruebas de calidad
 # Control automatico mediante pre-commit
+
+
+def calcular_descuento(precio):
+    descuento = 10
+    resultado = precio - descuento
+    return resultado
+
+
+def verificar_usuario(nombre):
+    return nombre == "admin"
